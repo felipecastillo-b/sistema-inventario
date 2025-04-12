97e462ba536bc180746f410d4ccf9ec3fcd0b319
Felipe Castillo <felipecastillo.snk@gmail.com>
2025-04-11 23:42:30 -0400

    enchance bbdd

--- server/prisma/migrations/20250412034032_inventory_system_bbdd/migration.sql
+++ server/prisma/migrations/20250412034032_inventory_system_bbdd/migration.sql
@@ -3,6 +3,10 @@ CREATE TABLE "Clients" (
     "clientId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Clients_pkey" PRIMARY KEY ("clientId")
 );
@@ -15,6 +19,8 @@ CREATE TABLE "Users" (
     "password" TEXT NOT NULL,
     "roleId" INTEGER NOT NULL,
     "statusId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("userId")
 );
@@ -30,7 +36,7 @@ CREATE TABLE "Roles" (
 
 -- CreateTable
 CREATE TABLE "Status" (
-    "statusId" SERIAL NOT NULL,
+    "statusId" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
 
@@ -40,21 +46,51 @@ CREATE TABLE "Status" (
 -- CreateTable
 CREATE TABLE "Products" (
     "productId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "supplierId" TEXT NOT NULL,
+    "statusId" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "rating" DOUBLE PRECISION,
     "stockQuantity" INTEGER NOT NULL,
     "stockMinimum" INTEGER NOT NULL,
     "image_url" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Products_pkey" PRIMARY KEY ("productId")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "categoryId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("categoryId")
+);
+
+-- CreateTable
+CREATE TABLE "Suppliers" (
+    "supplierId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Suppliers_pkey" PRIMARY KEY ("supplierId")
 );
 
 -- CreateTable
 CREATE TABLE "Sales" (
     "saleId" TEXT NOT NULL,
     "productId" TEXT NOT NULL,
-    "timestamp" TIMESTAMP(3) NOT NULL,
+    "clientId" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "quantity" INTEGER NOT NULL,
     "unitPrice" DOUBLE PRECISION NOT NULL,
     "totalAmount" DOUBLE PRECISION NOT NULL,
@@ -66,7 +102,7 @@ CREATE TABLE "Sales" (
 CREATE TABLE "Purchases" (
     "purchaseId" TEXT NOT NULL,
     "productId" TEXT NOT NULL,
-    "timestamp" TIMESTAMP(3) NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "quantity" INTEGER NOT NULL,
     "unitCost" DOUBLE PRECISION NOT NULL,
     "totalCost" DOUBLE PRECISION NOT NULL,
@@ -77,9 +113,10 @@ CREATE TABLE "Purchases" (
 -- CreateTable
 CREATE TABLE "Expenses" (
     "expenseId" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
+    "expenseByCategoryId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
     "amount" DOUBLE PRECISION NOT NULL,
-    "timestamp" TIMESTAMP(3) NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Expenses_pkey" PRIMARY KEY ("expenseId")
 );
@@ -89,7 +126,7 @@ CREATE TABLE "SalesSummary" (
     "salesSummaryId" TEXT NOT NULL,
     "totalValue" DOUBLE PRECISION NOT NULL,
     "changePercentage" DOUBLE PRECISION,
-    "date" TIMESTAMP(3) NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "SalesSummary_pkey" PRIMARY KEY ("salesSummaryId")
 );
@@ -99,7 +136,7 @@ CREATE TABLE "PurchaseSummary" (
     "purchaseSummaryId" TEXT NOT NULL,
     "totalPurchased" DOUBLE PRECISION NOT NULL,
     "changePercentage" DOUBLE PRECISION,
-    "date" TIMESTAMP(3) NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "PurchaseSummary_pkey" PRIMARY KEY ("purchaseSummaryId")
 );
@@ -108,7 +145,8 @@ CREATE TABLE "PurchaseSummary" (
 CREATE TABLE "ExpenseSummary" (
     "expenseSummaryId" TEXT NOT NULL,
     "totalExpenses" DOUBLE PRECISION NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
+    "changePercentage" DOUBLE PRECISION,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "ExpenseSummary_pkey" PRIMARY KEY ("expenseSummaryId")
 );
@@ -116,10 +154,10 @@ CREATE TABLE "ExpenseSummary" (
 -- CreateTable
 CREATE TABLE "ExpenseByCategory" (
     "expenseByCategoryId" TEXT NOT NULL,
-    "expenseSummaryId" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "amount" BIGINT NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "ExpenseByCategory_pkey" PRIMARY KEY ("expenseByCategoryId")
 );
@@ -137,10 +175,22 @@ ALTER TABLE "Users" ADD CONSTRAINT "Users_roleId_fkey" FOREIGN KEY ("roleId") RE
 ALTER TABLE "Users" ADD CONSTRAINT "Users_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("statusId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Products" ADD CONSTRAINT "Products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("categoryId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Products" ADD CONSTRAINT "Products_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "Suppliers"("supplierId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Products" ADD CONSTRAINT "Products_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("statusId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Sales" ADD CONSTRAINT "Sales_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("productId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Sales" ADD CONSTRAINT "Sales_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Clients"("clientId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Purchases" ADD CONSTRAINT "Purchases_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("productId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ExpenseByCategory" ADD CONSTRAINT "ExpenseByCategory_expenseSummaryId_fkey" FOREIGN KEY ("expenseSummaryId") REFERENCES "ExpenseSummary"("expenseSummaryId") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Expenses" ADD CONSTRAINT "Expenses_expenseByCategoryId_fkey" FOREIGN KEY ("expenseByCategoryId") REFERENCES "ExpenseByCategory"("expenseByCategoryId") ON DELETE RESTRICT ON UPDATE CASCADE;
